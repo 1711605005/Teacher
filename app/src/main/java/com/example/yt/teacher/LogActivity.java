2f0a7adf996cb46b8477c8291542e33d9adcf51a
qiudekai026 <1297637494@qq.com>
2019-01-03 10:24:02 +0800

    修改3

--- app/src/main/java/com/example/yt/teacher/LogActivity.java
+++ app/src/main/java/com/example/yt/teacher/LogActivity.java
@@ -3,10 +3,13 @@ package com.example.yt.teacher;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LogActivity extends AppCompatActivity {
     TextView tvName,tvNum,tvCourse,tvPhone,tvSex,tvDegree,tvEmile;
+    Button btnUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +31,23 @@ public class LogActivity extends AppCompatActivity {
         tvSex=findViewById(R.id.tv_log_sex);
         tvDegree=findViewById(R.id.tv_log_degree);
         tvEmile=findViewById(R.id.tv_log_e_mile);
+        btnUpdate=findViewById(R.id.btn_log_update);
 
         tvName.setText("姓名："+name);
-        tvNum.setText("编号:"+num);
-        tvCourse.setText("课程："+course);
-        tvPhone.setText("电话："+phone);
+        tvNum.setText("编号："+num);
+        tvCourse.setText("所教课程："+course);
+        tvPhone.setText("电话号码："+phone);
         tvSex.setText("性别："+sex);
-        tvDegree.setText("学历"+degree);
-        tvEmile.setText("e_mile"+e_mile);
+        tvDegree.setText("学历："+degree);
+        tvEmile.setText("邮箱："+e_mile);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(LogActivity.this,InsertActivity.class);
+                startActivity(intent1);
+
+            }
+        });
     }
 }
