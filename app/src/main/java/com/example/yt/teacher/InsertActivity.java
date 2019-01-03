17eeb11937e979403c6b6f8f2fe8bbd31853d3f2
qiudekai026 <1297637494@qq.com>
2019-01-03 10:00:21 +0800

    提交3

--- app/src/main/java/com/example/yt/teacher/InsertActivity.java
+++ app/src/main/java/com/example/yt/teacher/InsertActivity.java
@@ -1,13 +1,51 @@
 package com.example.yt.teacher;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class InsertActivity extends AppCompatActivity {
+    EditText edtTxtName,edtTxtNum,edtTxtCourse,edtTxtSex,edtTxtPhone,edtTxtDegree,edtTxtEmidle;
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+        intUi();
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                passDate();
+            }
+
+            private void passDate() {
+                Intent intent=new Intent(InsertActivity.this,LogActivity.class);
+                intent.putExtra("name",edtTxtName.getText().toString().trim());
+                intent.putExtra("num",edtTxtNum.getText().toString().trim());
+                intent.putExtra("course",edtTxtCourse.getText().toString().trim());
+                intent.putExtra("sex",edtTxtSex.getText().toString().trim());
+                intent.putExtra("phone",edtTxtPhone.getText().toString().trim());
+                intent.putExtra("degree",edtTxtDegree.getText().toString().trim());
+                intent.putExtra("e_mile",edtTxtEmidle.getText().toString().trim());
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void intUi() {
+        edtTxtName=findViewById(R.id.edt_insert_name);
+        edtTxtNum=findViewById(R.id.edt_insert_num);
+        edtTxtCourse=findViewById(R.id.edt_insert_course);
+        edtTxtSex=findViewById(R.id.edt_insert_sex);
+        edtTxtPhone=findViewById(R.id.edt_insert_phone);
+        edtTxtDegree=findViewById(R.id.edt_insert_degree);
+        edtTxtEmidle=findViewById(R.id.edt_insert_e_mile);
+        btnSave=findViewById(R.id.btn_insert_save);
     }
 }
--- app/src/main/java/com/example/yt/teacher/LogActivity.java
+++ app/src/main/java/com/example/yt/teacher/LogActivity.java
@@ -6,16 +6,35 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class LogActivity extends AppCompatActivity {
-    TextView tvname,tvid;
+    TextView tvName,tvNum,tvCourse,tvPhone,tvSex,tvDegree,tvEmile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
         Intent intent=getIntent();
-        String id=intent.getStringExtra("id");
-        tvid=findViewById(R.id.tv_log_id);
-        tvid.setText("编号:"+id);
+        String num=intent.getStringExtra("num");
+        String name=intent.getStringExtra("name");
+        String course=intent.getStringExtra("course");
+        String phone=intent.getStringExtra("phone");
+        String sex=intent.getStringExtra("sex");
+        String degree=intent.getStringExtra("degree");
+        String e_mile=intent.getStringExtra("e_mile");
 
+        tvName=findViewById(R.id.tv_log_name);
+        tvNum=findViewById(R.id.tv_log_num);
+        tvCourse=findViewById( R.id.tv_log_course);
+        tvPhone=findViewById(R.id.tv_log_phone);
+        tvSex=findViewById(R.id.tv_log_sex);
+        tvDegree=findViewById(R.id.tv_log_degree);
+        tvEmile=findViewById(R.id.tv_log_e_mile);
+
+        tvName.setText("姓名："+name);
+        tvNum.setText("编号:"+num);
+        tvCourse.setText("课程："+course);
+        tvPhone.setText("电话："+phone);
+        tvSex.setText("性别："+sex);
+        tvDegree.setText("学历"+degree);
+        tvEmile.setText("e_mile"+e_mile);
     }
 }
