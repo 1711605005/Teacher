5e86ccbcc4ff30bbae3ee9a5bc16eafffdea118d
qiudekai026 <1297637494@qq.com>
2019-01-03 09:21:40 +0800

    提交2

--- app/src/main/java/com/example/yt/teacher/LogActivity.java
+++ app/src/main/java/com/example/yt/teacher/LogActivity.java
@@ -1,13 +1,21 @@
 package com.example.yt.teacher;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LogActivity extends AppCompatActivity {
+    TextView tvname,tvid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        Intent intent=getIntent();
+        String id=intent.getStringExtra("id");
+        tvid=findViewById(R.id.tv_log_id);
+        tvid.setText("编号:"+id);
+
     }
 }
--- app/src/main/java/com/example/yt/teacher/MainActivity.java
+++ app/src/main/java/com/example/yt/teacher/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.yt.teacher;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtTxtId,edtTxtPassword;
+    Button btnLand,btnRegister,btnLand2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intUI();
+        btnLand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                passDate();
+            }
+
+            private void passDate() {
+                Intent intent=new Intent(MainActivity.this,LogActivity.class);
+                intent.putExtra("id",edtTxtId.getText().toString().trim());
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void intUI() {
+        edtTxtId=findViewById(R.id.edtTxt_main_id);
+        edtTxtPassword=findViewById(R.id.edtTxt_main_password);
+        btnLand=findViewById(R.id.btn_main_land);
+        btnLand2=findViewById(R.id.btn_main_land2);
+        btnRegister=findViewById(R.id.btn_main_register);
     }
 }
