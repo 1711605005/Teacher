7fe5db026f762323c9d6db5d789b9357ebcd5e2e
1711605005 <1762158607@qq.com>
2019-01-04 14:26:12 +0800

    MyHelper代码编写

--- app/src/main/java/com/example/yt/teacher/InsertActivity.java
+++ app/src/main/java/com/example/yt/teacher/InsertActivity.java
@@ -1,6 +1,8 @@
 package com.example.yt.teacher;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,43 +11,38 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class InsertActivity extends AppCompatActivity {
-    EditText edtTxtName,edtTxtNum,edtTxtCourse,edtTxtSex,edtTxtPhone,edtTxtDegree,edtTxtEmidle;
-    Button btnSave;
+    EditText edtTxtName,edtTxtNum,edtTxtCourse,edtTxtSex,edtTxtPhone,edtTxtDegree,edtTxtEmile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
-//        intUi();
-//        btnSave.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                passDate();
-//            }
-//
-//            private void passDate() {
-//                Intent intent=new Intent(InsertActivity.this,LogActivity.class);
-//                intent.putExtra("name",edtTxtName.getText().toString().trim());
-//                intent.putExtra("num",edtTxtNum.getText().toString().trim());
-//                intent.putExtra("course",edtTxtCourse.getText().toString().trim());
-//                intent.putExtra("sex",edtTxtSex.getText().toString().trim());
-//                intent.putExtra("phone",edtTxtPhone.getText().toString().trim());
-//                intent.putExtra("degree",edtTxtDegree.getText().toString().trim());
-//                intent.putExtra("e_mile",edtTxtEmidle.getText().toString().trim());
-//                startActivity(intent);
-//            }
-//        });
-//
-//    }
-//
-//    private void intUi() {
-//        edtTxtName=findViewById(R.id.edt_insert_name);
-//        edtTxtNum=findViewById(R.id.edt_insert_num);
-//        edtTxtCourse=findViewById(R.id.edt_insert_course);
-//        edtTxtSex=findViewById(R.id.edt_insert_sex);
-//        edtTxtPhone=findViewById(R.id.edt_insert_phone);
-//        edtTxtDegree=findViewById(R.id.edt_insert_degree);
-//        edtTxtEmidle=findViewById(R.id.edt_insert_e_mile);
-//        btnSave=findViewById(R.id.btn_insert_save);
+        edtTxtName=findViewById(R.id.edt_insert_name);
+        edtTxtNum=findViewById(R.id.edt_insert_num);
+        edtTxtSex=findViewById(R.id.edt_insert_sex);
+        edtTxtPhone=findViewById(R.id.edt_insert_phone);
+        edtTxtEmile=findViewById(R.id.edt_insert_emile);
+        edtTxtDegree=findViewById(R.id.edt_insert_degree);
+        edtTxtCourse=findViewById(R.id.edt_insert_course);
+        findViewById(R.id.btn_insert_save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name,degree,course,sex;
+                Long num,phone,emile;
+                SQLiteDatabase db;
+                ContentValues values;
+                switch (v.getId()){
+                    case R.id.btn_insert_save:
+                        name=edtTxtName.getText().toString();
+                        degree=edtTxtDegree.getText().toString();
+                        course=edtTxtCourse.getText().toString();
+                        sex=edtTxtSex.getText().toString();
+
+
+                }
+
+            }
+        });
+
     }
 }
