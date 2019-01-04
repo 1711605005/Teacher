ccfaa3067f75b54509459dbc65b70782cb9194ce
1711605005 <1762158607@qq.com>
2019-01-04 10:36:23 +0800

    界面修改与跳转代码编写

---
+++ app/src/main/java/com/example/yt/teacher/Infor.java
@@ -0,0 +1,62 @@
+package com.example.yt.teacher;
+
+public class Infor {
+    private String name,course,sex,degree;
+    private long num,phone,emile;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public long getNum() {
+        return num;
+    }
+
+    public void setNum(long num) {
+        this.num = num;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public long getEmile() {
+        return emile;
+    }
+
+    public void setEmile(long emile) {
+        this.emile = emile;
+    }
+}
